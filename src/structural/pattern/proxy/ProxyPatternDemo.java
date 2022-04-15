66c62de68817bc5f067c12cfa37bf0c55326eee8
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 16:07:19 -0500

    Se implementa patron de diseño de estructura Proxy.

--- src/structural/pattern/proxy/ProxyPatternDemo.java
+++ src/structural/pattern/proxy/ProxyPatternDemo.java
@@ -3,7 +3,15 @@ package structural.pattern.proxy;
 public class ProxyPatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Image image = new ProxyImage("test_10mb.jpg");
+
+		//image will be loaded from disk
+		image.display();
+		System.out.println("");
+
+		//image will not be loaded from disk
+		image.display();
 
 	}
 
