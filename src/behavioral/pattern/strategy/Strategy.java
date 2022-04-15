c56e3b857690ddf3a840a3e09c33f5dec4f187ff
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 17:46:49 -0500

    Se implementa patron de diseño de comportamiento Strategy.

---
+++ src/behavioral/pattern/strategy/Strategy.java
@@ -0,0 +1,5 @@
+package behavioral.pattern.strategy;
+
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
--- src/behavioral/pattern/strategy/StrategyPatternDemo.java
+++ src/behavioral/pattern/strategy/StrategyPatternDemo.java
@@ -3,7 +3,15 @@ package behavioral.pattern.strategy;
 public class StrategyPatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Context context = new Context(new OperationAdd());
+		System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+		context = new Context(new OperationSubstract());
+		System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+		context = new Context(new OperationMultiply());
+		System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
 
 	}
 
