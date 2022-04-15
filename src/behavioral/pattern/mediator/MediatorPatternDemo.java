fc1ef06aab9733e6d3446dc129f681bfbd2bf0cd
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 17:17:03 -0500

    Se implementa patron de diseño de comportamiento Mediator.

--- src/behavioral/pattern/mediator/MediatorPatternDemo.java
+++ src/behavioral/pattern/mediator/MediatorPatternDemo.java
@@ -3,7 +3,12 @@ package behavioral.pattern.mediator;
 public class MediatorPatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		User robert = new User("Robert");
+		User john = new User("John");
+
+		robert.sendMessage("Hi! John!");
+		john.sendMessage("Hello! Robert!");
 
 	}
 
