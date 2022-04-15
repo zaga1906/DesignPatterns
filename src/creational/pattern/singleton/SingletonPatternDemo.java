b85d518853eb1810ec8ab096cd55c171309100e0
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 13:19:17 -0500

    Se implementa patron de diseño de creacion Singleton.

--- src/creational/pattern/singleton/SingletonPatternDemo.java
+++ src/creational/pattern/singleton/SingletonPatternDemo.java
@@ -3,7 +3,16 @@ package creational.pattern.singleton;
 public class SingletonPatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		//illegal construct
+		//Compile Time Error: The constructor SingleObject() is not visible
+		//SingleObject object = new SingleObject();
+
+		//Get the only object available
+		SingleObject object = SingleObject.getInstance();
+
+		//show the message
+		object.showMessage();
 
 	}
 
