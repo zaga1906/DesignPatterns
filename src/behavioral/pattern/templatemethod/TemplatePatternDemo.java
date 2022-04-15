e6d07f74a78522fedf2a6c3c6bad27f19f5d7175
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 17:53:39 -0500

    Se implementa patron de diseño de comportamiento Template method.

--- src/behavioral/pattern/templatemethod/TemplatePatternDemo.java
+++ src/behavioral/pattern/templatemethod/TemplatePatternDemo.java
@@ -3,7 +3,12 @@ package behavioral.pattern.templatemethod;
 public class TemplatePatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Game game = new Cricket();
+		game.play();
+		System.out.println();
+		game = new Football();
+		game.play();
 
 	}
 
