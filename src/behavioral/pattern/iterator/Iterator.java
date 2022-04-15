a55ee6d879df5410eb25669360943e9ebfbf544a
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 17:11:52 -0500

    Se implementa patron de diseño de comportamiento Iterator.

---
+++ src/behavioral/pattern/iterator/Iterator.java
@@ -0,0 +1,6 @@
+package behavioral.pattern.iterator;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
--- src/behavioral/pattern/iterator/IteratorPatternDemo.java
+++ src/behavioral/pattern/iterator/IteratorPatternDemo.java
@@ -3,7 +3,13 @@ package behavioral.pattern.iterator;
 public class IteratorPatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		NameRepository namesRepository = new NameRepository();
+
+		for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
+			String name = (String)iter.next();
+			System.out.println("Name : " + name);
+		}
 
 	}
 
