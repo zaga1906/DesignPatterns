bcf6639453c6e3cc3dc44601f9efa7c42bc85a52
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 15:09:51 -0500

    Se implementa patron de diseño de estructura Bridge.

--- src/structural/pattern/bridge/BridgePatternDemo.java
+++ src/structural/pattern/bridge/BridgePatternDemo.java
@@ -3,7 +3,12 @@ package structural.pattern.bridge;
 public class BridgePatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Shape redCircle = new Circle(100,100, 10, new RedCircle());
+		Shape greenCircle = new Circle(100,100, 10, new GreenCircle());
+
+		redCircle.draw();
+		greenCircle.draw();
 
 	}
 
---
+++ src/structural/pattern/bridge/DrawAPI.java
@@ -0,0 +1,5 @@
+package structural.pattern.bridge;
+
+public interface DrawAPI {
+    public void drawCircle(int radius, int x, int y);
+}
