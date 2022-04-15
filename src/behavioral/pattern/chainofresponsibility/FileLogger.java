b5adbdf9c4df4943ed18884061b288d2ea209ce7
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 16:22:12 -0500

    Se implementa patron de diseño de comportamiento Chain Of Responsibility.

---
+++ src/behavioral/pattern/chainofresponsibility/FileLogger.java
@@ -0,0 +1,13 @@
+package behavioral.pattern.chainofresponsibility;
+
+public class FileLogger extends AbstractLogger{
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
