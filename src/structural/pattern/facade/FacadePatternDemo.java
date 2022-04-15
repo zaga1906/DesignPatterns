4ce4937087404df362b4e9de91ad3d3a282ce3e6
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 15:41:30 -0500

    Se implementa patron de diseño de estructura Facade.

--- src/structural/pattern/facade/FacadePatternDemo.java
+++ src/structural/pattern/facade/FacadePatternDemo.java
@@ -3,7 +3,12 @@ package structural.pattern.facade;
 public class FacadePatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ShapeMaker shapeMaker = new ShapeMaker();
+
+		shapeMaker.drawCircle();
+		shapeMaker.drawRectangle();
+		shapeMaker.drawSquare();
 
 	}
 
---
+++ src/structural/pattern/facade/Shape.java
@@ -0,0 +1,5 @@
+package structural.pattern.facade;
+
+public interface Shape {
+    void draw();
+}
