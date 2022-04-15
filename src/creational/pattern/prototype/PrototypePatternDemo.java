a147012edd9bd5efe3b969c48e619fd78a860c84
SURAMERICANA\edgapaca <eeparra@sura.com.co>
2022-04-15 14:04:32 -0500

    Se implementa patron de diseño de creacion Prototype.

--- src/creational/pattern/prototype/PrototypePatternDemo.java
+++ src/creational/pattern/prototype/PrototypePatternDemo.java
@@ -3,7 +3,17 @@ package creational.pattern.prototype;
 public class PrototypePatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ShapeCache.loadCache();
+
+		Shape clonedShape = (Shape) ShapeCache.getShape("1");
+		System.out.println("Shape : " + clonedShape.getType());
+
+		Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+		System.out.println("Shape : " + clonedShape2.getType());
+
+		Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+		System.out.println("Shape : " + clonedShape3.getType());
 
 	}
 
---
+++ src/creational/pattern/prototype/Square.java
@@ -0,0 +1,13 @@
+package creational.pattern.prototype;
+
+public class Square extends Shape{
+
+    public Square(){
+        type = "Square";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
